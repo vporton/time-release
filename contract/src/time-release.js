32670b3cfe28a13978246717ef7fae89abf7302c
Victor Porton <porton@narod.ru>
2020-04-21 10:59:33 +0300

    contract/src/time-release.js

---
+++ contract/src/time-release.js
@@ -0,0 +1,34 @@
+export class BaseTimeRelease extends Purse {
+    constructor(periods, periodLength, amount, initiated = Date.now()) {
+        this.periods = periods;
+        this.periodLength = periodLength;
+        this.amount = amount;
+        this.initiated = initiated;
+        // FIXME: harden
+    }
+    getCurrentAmount() {
+        fullPeriodsPassed = Math.floor((currentTime() - this.initiated) / this.periodLength);
+        if(fullPeriodsPassed < _transfer.lockedForPeriods) return 0;
+        if(fullPeriodsPassed >= _transfer.vestedForPeriods) fullPeriodsPassed = _transfer.vestedForPeriods - 1;
+        perPeriod = _transfer.amount / _transfer.vestedForPeriods;
+        return this.periodAmount * (fullPeriodsPassed + 1); // FIXME: math
+    }
+    // withdraw(amount) {        
+    // }
+    currentTime() { }
+}
+
+export class TimeRelease extends BaseTimeRelease {
+    currentTime() {
+        return Date.now();
+    }
+}
+
+export class TestTimeRelease extends BaseTimeRelease {
+    setCurrentTime(time) {
+        this._currentTime = time;
+    }
+    currentTime() {
+        return this._currentTime;
+    }
+}
