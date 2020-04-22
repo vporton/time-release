ecc2ec9278d06ceb058ae8befbd834e6c15789cb
Victor Porton <porton@narod.ru>
2020-04-23 02:26:06 +0300

    bug fix

--- contract/src/contracts/time-release.js
+++ contract/src/contracts/time-release.js
@@ -1,10 +1,8 @@
-import { makeZoe } from '@agoric/zoe';
 import { E } from '@agoric/eventual-send';
 
 class _TimeRelease {
-    // TODO: Pass `zcf` to it, remove `E()`.
     // FIXME: Needs `E()` for timerService.
-    constructor(payment, lockedUntil = Date.now()) {
+    constructor(zcf, payment, lockedUntil = Date.now()) {
         let _offer = null;
         let _payment = payment;
         let _lockedUntil = lockedUntil;
@@ -12,8 +10,8 @@ class _TimeRelease {
             return _lockedUntil;
         }
         this.getPayment = function() {
-            const zoe = makeZoe({ require });
-            return _offer && E(zoe).isOfferActive(_offer) && Date.now() >= _lockedUntil ? _payment : null;
+            const zoe = zcf.getZoeService();
+            return _offer && zoe.isOfferActive(_offer) && Date.now() >= _lockedUntil ? _payment : null;
         }
         // SECURITY: Don't forget to call this function,
         // otherwise getPayment() will always return null.
@@ -25,6 +23,6 @@ class _TimeRelease {
 
 _TimeRelease = harden(_TimeRelease);
 
-export function makeTimeRelease(payment, lockedUntil = Date.now()) {
-    return harden(new _TimeRelease(payment, lockedUntil));
+export function makeTimeRelease(zcf, payment, lockedUntil = Date.now()) {
+    return harden(new _TimeRelease(zcf, payment, lockedUntil));
 }
