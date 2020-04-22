9962930cc6393c62a839fdbb318715aa1dd70f37
Victor Porton <porton@narod.ru>
2020-04-22 10:20:34 +0300

    more contracts

---
+++ contract/src/contracts/time-release-test.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-use-before-define */
+import harden from '@agoric/harden';
+import produceIssuer from '@agoric/ertp';
+import { makeZoeHelpers } from '/home/porton/Projects/bounties/agoric-sdk/packages/zoe/src/contractSupport'; // FIXME
+
+import './time-release';
+
+// zcf is the Zoe Contract Facet, i.e. the contract-facing API of Zoe
+export const makeContract = harden(zcf => {
+  // Create the internal token mint
+  const currencyIssuer = produceIssuer('BaytownBucks')
+  const { mint: baytownBucksMint, issuer } = currencyIssuer;
+  const baytownBucks = issuer.getAmountMath().make;
+
+  let auditoriumOfferHandle;
+
+  return zcf.addNewIssuer(issuer, 'Ticket').then(() => {
+    // Mint the tickets ahead-of-time (instead of on-demand)
+    // This way, they can be passed to Zoe + ERTP who will be doing the bookkeeping
+    // of which tickets have been sold and which tickets are still for sale
+    const ticketsAmount = baytownBucks(1000);
+    // const ticketsPayment = baytownBucksMint.mintPayment(ticketsAmount);
+
+    // the contract creates an offer {give: tickets, want: nothing} with the tickets
+    const offerHook = userOfferHandle => {
+      const ticketsAmount = baytownBucks(1000);
+      const ticketsPayment = baytownBucksMint.mintPayment(ticketsAmount);
+      let tempContractHandle;
+      const contractSelfInvite = zcf.makeInvitation(
+        offerHandle => (tempContractHandle = offerHandle),
+      );
+      zcf
+        .getZoeService()
+        .offer(
+          contractSelfInvite,
+          harden({ give: { Ticket: ticketsAmount } }),
+          harden({ Ticket: ticketsPayment }),
+        ).then(() => {
+          zcf.reallocate(
+            [tempContractHandle, userOfferHandle],
+            [
+              zcf.getCurrentAllocation(userOfferHandle),
+              zcf.getCurrentAllocation(tempContractHandle),
+            ],
+          );
+          zcf.complete([tempContractHandle, userOfferHandle]); // FIXME: enough just one of them?
+          return `Payment scheduled.`;
+        });
+    }
+    return harden({
+      invite: zcf.makeInvitation(offerHook),
+      publicAPI: {
+        invite2: zcf.makeInvitation(offerHook), // FIXME
+        currency: baytownBucks, // FIXME: security
+        issuer: issuer,
+      },
+    });
+  });
+});
---
+++ contract/test/test-time-release-test.js
@@ -0,0 +1,66 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { test } from 'tape-promise/tape';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import bundleSource from '@agoric/bundle-source';
+import harden from '@agoric/harden';
+import produceIssuer from '@agoric/ertp';
+import { E } from '@agoric/eventual-send';
+
+import { makeZoe } from '@agoric/zoe';
+
+const operaConcertTicketRoot = `${__dirname}/../src/contracts/time-release-test.js`;
+
+test(`Zoe opera ticket contract`, async t => {
+  // Setup initial conditions
+  const zoe = makeZoe({ require });
+  const inviteIssuer = zoe.getInviteIssuer();
+ 
+  const contractReadyP = bundleSource(operaConcertTicketRoot).then(
+    ({ source, moduleFormat }) => {
+      const installationHandle = zoe.install(source, moduleFormat);
+
+      return zoe
+        .makeInstance(installationHandle)
+        .then(myInvite => {
+          return inviteIssuer
+            .getAmountOf(myInvite)
+            .then(({ extent: [{ instanceHandle: auditoriumHandle }] }) => {
+              const { publicAPI } = zoe.getInstanceRecord(auditoriumHandle);
+
+              return (
+                zoe
+                  .offer(myInvite, harden({}))
+                  // cancel will be renamed complete: https://github.com/Agoric/agoric-sdk/issues/835
+                  // cancelObj exists because of a current limitation in @agoric/marshal : https://github.com/Agoric/agoric-sdk/issues/818
+                  .then(
+                    async ({
+                      outcome: outcomeP,
+                      payout,
+                      cancelObj: { cancel: complete },
+                      offerHandle,
+                    }) => {
+                      const amount = await E(publicAPI.issuer).getAmountOf((await payout).Ticket);
+                      t.equal(
+                        amount.extent,
+                        1000,
+                        `got 1000 tokens`,
+                      );
+
+                      return {
+                        publicAPI,
+                        operaPayout: payout,
+                        complete,
+                      };
+                    },
+                  )
+              );
+            });
+        });
+    },
+  )
+  .catch(err => {
+    console.error('Error in last Opera part', err);
+    t.fail('  error');
+  })
+  .then(() => t.end());
+});
