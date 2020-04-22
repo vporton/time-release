cd5b51e94f6e8aabb5c2094adb75c2d80dbf3b76
Victor Porton <porton@narod.ru>
2020-04-22 09:12:21 +0300

    testing

--- contract/test/test-contract.js
+++ contract/test/test-contract.js
@@ -8,41 +8,33 @@ import { E } from '@agoric/eventual-send';
 
 import { makeZoe } from '@agoric/zoe';
 
-const operaConcertTicketRoot = `${__dirname}/../src/contracts/operaConcertTicket.js`;
+const operaConcertTicketRoot = `${__dirname}/../src/contracts/contract.js`;
 
 test(`Zoe opera ticket contract`, async t => {
   // Setup initial conditions
   const zoe = makeZoe({ require });
   const inviteIssuer = zoe.getInviteIssuer();
-
-  // === Initial Opera de Bordeaux part ===
+ 
   const contractReadyP = bundleSource(operaConcertTicketRoot).then(
     ({ source, moduleFormat }) => {
       const installationHandle = zoe.install(source, moduleFormat);
 
       return zoe
         .makeInstance(installationHandle)
-        .then(auditoriumInvite => {
+        .then(myInvite => {
           return inviteIssuer
-            .getAmountOf(auditoriumInvite)
+            .getAmountOf(myInvite)
             .then(({ extent: [{ instanceHandle: auditoriumHandle }] }) => {
               const { publicAPI } = zoe.getInstanceRecord(auditoriumHandle);
 
-              // The auditorium makes an offer.
               return (
-                // Note that the proposal here is empty
-                // This is due to a current limitation in proposal expressivness: https://github.com/Agoric/agoric-sdk/issues/855
-                // It's impossible to know in advance how many tickets will be sold, so it's not possible
-                // to say `want: moola(3*22)`
-                // in a future version of Zoe, it will be possible to express:
-                // "i want n times moolas where n is the number of sold tickets"
                 zoe
-                  .offer(auditoriumInvite, harden({}))
+                  .offer(myInvite, harden({}))
                   // cancel will be renamed complete: https://github.com/Agoric/agoric-sdk/issues/835
                   // cancelObj exists because of a current limitation in @agoric/marshal : https://github.com/Agoric/agoric-sdk/issues/818
                   .then(
                     async ({
-                      outcome: auditoriumOutcomeP,
+                      outcome: outcomeP,
                       payout,
                       cancelObj: { cancel: complete },
                       offerHandle,
@@ -50,6 +42,13 @@ test(`Zoe opera ticket contract`, async t => {
                       const { currentAllocation } = await E(zoe).getOffer(
                         await offerHandle,
                       );
+
+                      console.log("currentAllocation:", currentAllocation);
+                      // t.equal(
+                      //   currentAllocation.Token.extent,
+                      //   1000,
+                      //   `got 1000 tokens`,
+                      // );
 
                       return {
                         publicAPI,
@@ -64,21 +63,21 @@ test(`Zoe opera ticket contract`, async t => {
     },
   )
 
-  return contractReadyP.then(({ publicAPI, operaPayout, complete }) => {
-    const ticketAmountMath = publicAPI.currency(1000);
+  // return contractReadyP.then(({ publicAPI, operaPayout, complete }) => {
+  //   const ticketAmountMath = publicAPI.currency(1000);
 
-    const done = operaPayout.then(payout => {
-      return payout.Money.then(moneyPayment => {
-        return operaPurse.deposit(moneyPayment);
-      }).then(() => {
-        console.log(operaPurse.getCurrentAmount().extent)
-      });
-    });
+  //   const done = operaPayout.then(payout => {
+  //     return payout.Money.then(moneyPayment => {
+  //       return operaPurse.deposit(moneyPayment);
+  //     }).then(() => {
+  //       console.log(operaPurse.getCurrentAmount().extent)
+  //     });
+  //   });
 
-    complete();
+  //   complete();
 
-    return done;
-  })
+  //   return done;
+  // })
   .catch(err => {
     console.error('Error in last Opera part', err);
     t.fail('  error');
