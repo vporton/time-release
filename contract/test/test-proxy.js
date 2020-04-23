de5e87a1fdb5606e23e87e895662ef458c5989a3
Victor Porton <porton@narod.ru>
2020-04-23 07:19:04 +0300

    comment bug

--- contract/test/test-proxy.js
+++ contract/test/test-proxy.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { test } from 'tape-promise/tape';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import bundleSource from '@agoric/bundle-source';
 import harden from '@agoric/harden';
 import produceIssuer from '@agoric/ertp';
