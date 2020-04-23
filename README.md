4e568424e9091744b98540e78c7c163534cc2dc7
Victor Porton <porton@narod.ru>
2020-04-24 00:11:25 +0300

    misc

--- README.md
+++ README.md
@@ -1,28 +1,51 @@
 This repository contains the `proxy.js` smart contract that
 locks the money under a certain time moment.
 
+The contract first receives the money together with a unique handle (or secret key),
+and then it can be withdrawn by anybody with the key under the condition that enough
+time passed.
+
+When withdrawn, the extent of the payment contains an object that contains another
+payment (in a private variable). This "internal" payment could be obtained (as shown
+in the test) and then withdrawn.
+
+# Notes
+
+This contract is intented to be deployed once and used from other contracts
+(who would receive `registry` in `terms` to be able to obtain our contract).
+
+# Deployment
+
+```
+agoric deploy contract/deploy.js
+```
+
 # Usage
 
 Sender:
 
-    const handle = // unique handle (to disallow users not having the handle to withdraw our payment)
-    const data = // date of the payment  
-    const sendInvite = inviteIssuer.claim(publicAPI.makeSendInvite(harden(payment), harden(handle), harden(date))());
-    const aliceProposal = {};
-    return zoe
+```
+const handle = // unique handle (to disallow users not having the handle to withdraw our payment)
+const date = // date of the payment  
+const sendInvite = inviteIssuer.claim(publicAPI.makeSendInvite(harden(payment), harden(handle), harden(date))());
+const aliceProposal = {};
+return zoe
     .offer(sendInvite, harden(aliceProposal), {})
+```
 
 Receiver:
 
-    const receiveInvite = inviteIssuer.claim(publicAPI.makeReceiveInvite(handle)());
-    const bobProposal = {}
-    return zoe
+```
+const receiveInvite = inviteIssuer.claim(publicAPI.makeReceiveInvite(handle)());
+const bobProposal = {}
+return zoe
     .offer(receiveInvite, harden(bobProposal), {})
-    // ...
-    const wrapperPayment = await (await payout).Wrapper;
-    const amount = await E(publicAPI.issuer).getAmountOf(wrapperPayment);
-    const payment = await E(publicAPI.issuer).getAmountOf(amount.extent[0][0]);
-    const timeRelease = payment.extent[0][0];
-    const realPayment = await timeRelease.getPayment()
+// ...
+const wrapperPayment = await (await payout).Wrapper;
+const amount = await E(publicAPI.issuer).getAmountOf(wrapperPayment);
+const payment = await E(publicAPI.issuer).getAmountOf(amount.extent[0][0]);
+const timeRelease = payment.extent[0][0];
+const realPayment = await timeRelease.getPayment()
+```
 
 Now `realPayment` is either the payment we can receive or null if the time has not yet come.
--- ui/public/src/main.js
+++ ui/public/src/main.js
@@ -13,14 +13,9 @@ const selects = {
   $tipPurse: /** @type {HTMLSelectElement} */ (document.getElementById('tipPurse')),
 };
 
-const $forFree = document.getElementById('forFree');
 const $forTip = document.getElementById('forTip');
 
 export default async function main() {
-  selects.$brands.addEventListener('change', () => {
-    flipSelectedBrands(selects);
-  });
-  
   /**
    * @param {{ type: string; data: any; }} obj
    */
@@ -39,8 +34,6 @@ export default async function main() {
     }
   };
 
-  const $numEncouragements = /** @type {HTMLInputElement} */ (document.getElementById('numEncouragements'));
-
   /**
    * @param {{ type: string; data: any; }} obj
    */
@@ -50,7 +43,6 @@ export default async function main() {
         alert(`Encourager says: ${obj.data}`);
         break;
       case 'encouragement/encouragedResponse':
-        $numEncouragements.innerHTML = obj.data.count;
         break;
     }
   };
@@ -63,52 +55,41 @@ export default async function main() {
   });
 
   const apiSend = await connect('api', apiRecv).then(apiSend => {
-    apiSend({
-      type: 'encouragement/subscribeNotifications',
-    });
-
     $encourageMe.removeAttribute('disabled');
     $encourageMe.addEventListener('click', () => {
-      if ($forFree.checked) {
-        apiSend({
-          type: 'encouragement/getEncouragement',
-        });
-      }
-      if ($forTip.checked) {
-        const now = Date.now();
-        const offer = {
-          // JSONable ID for this offer.  This is scoped to the origin.
-          id: now,
-      
-          // Contract-specific metadata.
-          instanceRegKey: INSTANCE_REG_KEY,
-      
-          // Format is:
-          //   hooks[targetName][hookName] = [hookMethod, ...hookArgs].
-          // Then is called within the wallet as:
-          //   E(target)[hookMethod](...hookArgs)
-          hooks: {
-            publicAPI: {
-              getInvite: ['makeInvite'], // E(publicAPI).makeInvite()
+      const now = Date.now();
+      const offer = {
+        // JSONable ID for this offer.  This is scoped to the origin.
+        id: now,
+    
+        // Contract-specific metadata.
+        instanceRegKey: INSTANCE_REG_KEY,
+    
+        // Format is:
+        //   hooks[targetName][hookName] = [hookMethod, ...hookArgs].
+        // Then is called within the wallet as:
+        //   E(target)[hookMethod](...hookArgs)
+        hooks: {
+          publicAPI: {
+            getInvite: ['makeInvite'], // E(publicAPI).makeInvite()
+          },
+        },
+    
+        proposalTemplate: {
+          give: {
+            Tip: {
+              // The pursePetname identifies which purse we want to use
+              pursePetname: 'Fun budget',
+              extent: 1,
             },
           },
-      
-          proposalTemplate: {
-            give: {
-              Tip: {
-                // The pursePetname identifies which purse we want to use
-                pursePetname: 'Fun budget',
-                extent: 1,
-              },
-            },
-            exit: { onDemand: null },
-          },
-        };
-        walletSend({
-          type: 'walletAddOffer',
-          data: offer
-        });
-      }
+          exit: { onDemand: null },
+        },
+      };
+      walletSend({
+        type: 'walletAddOffer',
+        data: offer
+      });
     });
     
     return apiSend;
