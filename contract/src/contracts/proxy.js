a35f4e1f7a7f95b2f5c9f93a0defe4c3c674faa8
Victor Porton <porton@narod.ru>
2020-05-01 20:26:46 +0300

    using kate's code

--- contract/src/contracts/proxy.js
+++ contract/src/contracts/proxy.js
@@ -1,172 +1,213 @@
-/* eslint-disable no-use-before-define */
+// /* eslint-disable no-use-before-define */
+// import harden from '@agoric/harden';
+// import produceIssuer from '@agoric/ertp';
+// import { makeZoeHelpers } from '@agoric/zoe/src/contractSupport';
+
+// import { makeTimeRelease } from './time-release';
+// import { cleanProposal } from '@agoric/zoe/src/cleanProposal';
+
+
+// // zcf is the Zoe Contract Facet, i.e. the contract-facing API of Zoe
+// /**
+//  * @type {import('@agoric/zoe').MakeContract}
+//  */
+// export const makeContract = harden(zcf => {
+//   const { terms: { timerService } = {} } = zcf.getInstanceRecord();
+
+//   let nonce = 0;
+
+//   const { issuer: issuer0, mint: mint0, amountMath: amountMath0 } = produceIssuer(
+//     'Future',
+//     'set',
+//   );
+//   // const wrapperToken0 = amountMath0.make;
+
+//   const createToken = (issuer0) => {
+//     // Create the token mint
+//     const { issuer: wrapperIssuer } = produceIssuer(
+//       'Wrapper' + ++nonce,
+//       'set',
+//     );
+
+//     return zcf.addNewIssuer(issuer0, 'Wrapper' + nonce)/*.then(afterDynamicallyAddingNewIssuer)*/
+//       .then(() => {
+//         return { issuer: wrapperIssuer, nonce };
+//       });
+//   };
+
+//   let tempContractHandle;
+//   const contractSelfInvite = zcf.makeInvitation(
+//     offerHandle => (tempContractHandle = offerHandle),
+//   );
+
+//   const receiveHook = inviteToken => async userOfferHandle => {
+//     const extent = inviteToken.extent;
+//     if(await E(_timerService).getCurrentTimestamp() < extent.date) {
+//       zcf.rejectOffer(userOfferHandle);
+//       return;
+//     }
+//     zcf.reallocate(
+//       [tempContractHandle, userOfferHandle],
+//       [
+//         zcf.getCurrentAllocation(userOfferHandle),
+//         zcf.getCurrentAllocation(tempContractHandle),
+//       ],
+//     );
+//     zcf.complete([tempContractHandle, userOfferHandle]); // FIXME: enough just one of them?
+//   };
+
+//   // the contract creates an offer {give: [nonce], want: nothing} with the time release wrapper
+//   const sendHook = (nonce, receiver, date) => async userOfferHandle => {
+//     //nonces.set(nonce, receiver);
+
+//     const lockedAmount = zcf.getCurrentAllocation(userOfferHandle); // paymentIssuer.getAmountOf(lockedPayment);
+
+//     // const wrapperAmountMath = wrapperIssuer.getAmountMath();
+//     // const wrapperToken = wrapperAmountMath.make;
+
+//     // const lock = makeTimeRelease(zcf, timerService, paymentIssuer, lockedPayment, date);
+//     // payments.set(receiver, lock);
+
+//     // const wrapperAmount = wrapperToken0(harden([nonce]));
+//     // const wrapperPayment = mint0.mintPayment(wrapperAmount);
+  
+//     var give = {}; // TODO
+//     give/*['Wrapper' + nonce]*/ = zcf.getCurrentAllocation(userOfferHandle, [ 'Wrapper' + nonce ]);
+//     // var paymentDesc = {};
+//     // paymentDesc['Wrapper' + nonce] = lockedPayment;
+//     await zcf
+//       .getZoeService()
+//       .offer(
+//         contractSelfInvite,
+//         harden({ /*want: want*/ }),
+//         harden({}),
+//       ).then(async () => {
+//         // Don't forget to call this, otherwise the other side won't be able to get the money:
+//         //lock.setOffer(tempContractHandle); // FIXME: Remove
+
+//         // const unique = {};
+        
+//         // a self-referential structure
+//         let record = {
+//           // offerHook: receiveHook(record),
+//           customProperties: { inviteDesc: 'offer' },
+//           // unique: unique,
+//           extent: {
+//             nonce: nonce,
+//             amount: lockedAmount,
+//             date: date,
+//           },
+//           expected: {
+//             give: give,
+//           },
+//         };
+//         record.offerHook = receiveHook(record);
+
+//         const inviteToken =
+//           inviteAnOffer(
+//             harden(record),
+//           );
+//         console.log(inviteToken)
+
+//         await receiver.receivePayment(inviteToken);
+          
+//       //   await zcf
+//       //     .getZoeService()
+//       //     .offer(
+//       //       contractSelfInvite,
+//       //       harden({ give: { Future: inviteToken } }), // TODO: Describe the payment here.
+//       //       harden({ Future: wrapperPayment }),
+//       //     ).then(async () => {
+//       //       // Don't forget to call this, otherwise the other side won't be able to get the money:
+//       //       //lock.setOffer(tempContractHandle);
+
+//       //       //receiver.receivePayment(receiverWrapperPayment)
+//       //       zcf.reallocate(
+//       //         [tempContractHandle, userOfferHandle],
+//       //         [
+//       //           zcf.getCurrentAllocation(userOfferHandle),
+//       //           zcf.getCurrentAllocation(tempContractHandle),
+//       //         ],
+//       //       );
+//       //       zcf.complete([tempContractHandle, userOfferHandle]); // FIXME: enough just one of them?
+//       //       return `Future sent.`;
+//       // });
+
+//         zcf.reallocate(
+//           [tempContractHandle, userOfferHandle],
+//           [
+//             zcf.getCurrentAllocation(userOfferHandle),
+//             zcf.getCurrentAllocation(tempContractHandle),
+//           ],
+//         );
+//         zcf.complete([tempContractHandle, userOfferHandle]); // FIXME: enough just one of them?
+
+//         var give = {};
+//         give['Wrapper' + nonce] = lockedAmount;
+//     });
+
+//   };
+
+//   const { inviteAnOffer } = makeZoeHelpers(zcf);   
+
+//   const adminHook = userOfferHandle => {
+//   }
+
+//   const makeSendInvite = (nonce, receiver, date) => () =>
+//     inviteAnOffer(
+//       harden({
+//         offerHook: sendHook(nonce, receiver, date),
+//         customProperties: { inviteDesc: 'offer' },
+//       }),
+//     );
+
+//   return harden({
+//     invite: zcf.makeInvitation(adminHook),
+//     publicAPI: {
+//       createToken,
+//       makeSendInvite,
+//       futureIssuer: issuer0,
+//     },
+//   });
+// });
+
+////////////////////////////////////////////////////////////
+
 import harden from '@agoric/harden';
-import produceIssuer from '@agoric/ertp';
+
+// Eventually will be importable from '@agoric/zoe-contract-support'
 import { makeZoeHelpers } from '@agoric/zoe/src/contractSupport';
 
-import { makeTimeRelease } from './time-release';
-import { cleanProposal } from '@agoric/zoe/src/cleanProposal';
-
+// Alice escrows funds, and then Bob can get them as a payout, but
+// only after a certain time.
 
 // zcf is the Zoe Contract Facet, i.e. the contract-facing API of Zoe
-/**
- * @type {import('@agoric/zoe').MakeContract}
- */
 export const makeContract = harden(zcf => {
-  const { terms: { timerService } = {} } = zcf.getInstanceRecord();
+  const { swap, assertKeywords } = makeZoeHelpers(zcf);
+  assertKeywords(harden(['Token']));
 
-  let nonce = 0;
+  const { terms: { timer }} = zcf.getInstanceRecord();
 
-  const { issuer: issuer0, mint: mint0, amountMath: amountMath0 } = produceIssuer(
-    'Future',
-    'set',
-  );
-  // const wrapperToken0 = amountMath0.make;
+  const makeClaimAssetsInvite = addAssetsOfferHandle => {
+    const claimAssetsOfferHook = claimAssetsOfferHandle => {
+      // TODO: use the timer to ensure that this reallocation and completion
+      // happens only after a certain time.
+      return swap(addAssetsOfferHandle, claimAssetsOfferHandle);
+    };
 
-  const createToken = (issuer0) => {
-    // Create the token mint
-    const { issuer: wrapperIssuer } = produceIssuer(
-      'Wrapper' + ++nonce,
-      'set',
+    return zcf.makeInvitation(
+      claimAssetsOfferHook,
+      harden({ inviteDesc: 'claimAssets' }),
     );
-
-    return zcf.addNewIssuer(issuer0, 'Wrapper' + nonce)/*.then(afterDynamicallyAddingNewIssuer)*/
-      .then(() => {
-        return { issuer: wrapperIssuer, nonce };
-      });
   };
 
-  let tempContractHandle;
-  const contractSelfInvite = zcf.makeInvitation(
-    offerHandle => (tempContractHandle = offerHandle),
-  );
-
-  const receiveHook = inviteToken => async userOfferHandle => {
-    const extent = inviteToken.extent;
-    if(await E(_timerService).getCurrentTimestamp() < extent.date) {
-      zcf.rejectOffer(userOfferHandle);
-      return;
-    }
-    zcf.reallocate(
-      [tempContractHandle, userOfferHandle],
-      [
-        zcf.getCurrentAllocation(userOfferHandle),
-        zcf.getCurrentAllocation(tempContractHandle),
-      ],
-    );
-    zcf.complete([tempContractHandle, userOfferHandle]); // FIXME: enough just one of them?
-  };
-
-  // the contract creates an offer {give: [nonce], want: nothing} with the time release wrapper
-  const sendHook = (nonce, receiver, date) => async userOfferHandle => {
-    //nonces.set(nonce, receiver);
-
-    const lockedAmount = zcf.getCurrentAllocation(userOfferHandle); // paymentIssuer.getAmountOf(lockedPayment);
-
-    // const wrapperAmountMath = wrapperIssuer.getAmountMath();
-    // const wrapperToken = wrapperAmountMath.make;
-
-    // const lock = makeTimeRelease(zcf, timerService, paymentIssuer, lockedPayment, date);
-    // payments.set(receiver, lock);
-
-    // const wrapperAmount = wrapperToken0(harden([nonce]));
-    // const wrapperPayment = mint0.mintPayment(wrapperAmount);
-  
-    var give = {}; // TODO
-    give/*['Wrapper' + nonce]*/ = zcf.getCurrentAllocation(userOfferHandle, [ 'Wrapper' + nonce ]);
-    // var paymentDesc = {};
-    // paymentDesc['Wrapper' + nonce] = lockedPayment;
-    await zcf
-      .getZoeService()
-      .offer(
-        contractSelfInvite,
-        harden({ /*want: want*/ }),
-        harden({}),
-      ).then(async () => {
-        // Don't forget to call this, otherwise the other side won't be able to get the money:
-        //lock.setOffer(tempContractHandle); // FIXME: Remove
-
-        // const unique = {};
-        
-        // a self-referential structure
-        let record = {
-          // offerHook: receiveHook(record),
-          customProperties: { inviteDesc: 'offer' },
-          // unique: unique,
-          extent: {
-            nonce: nonce,
-            amount: lockedAmount,
-            date: date,
-          },
-          expected: {
-            give: give,
-          },
-        };
-        record.offerHook = receiveHook(record);
-
-        const inviteToken =
-          inviteAnOffer(
-            harden(record),
-          );
-        console.log(inviteToken)
-
-        await receiver.receivePayment(inviteToken);
-          
-      //   await zcf
-      //     .getZoeService()
-      //     .offer(
-      //       contractSelfInvite,
-      //       harden({ give: { Future: inviteToken } }), // TODO: Describe the payment here.
-      //       harden({ Future: wrapperPayment }),
-      //     ).then(async () => {
-      //       // Don't forget to call this, otherwise the other side won't be able to get the money:
-      //       //lock.setOffer(tempContractHandle);
-
-      //       //receiver.receivePayment(receiverWrapperPayment)
-      //       zcf.reallocate(
-      //         [tempContractHandle, userOfferHandle],
-      //         [
-      //           zcf.getCurrentAllocation(userOfferHandle),
-      //           zcf.getCurrentAllocation(tempContractHandle),
-      //         ],
-      //       );
-      //       zcf.complete([tempContractHandle, userOfferHandle]); // FIXME: enough just one of them?
-      //       return `Future sent.`;
-      // });
-
-        zcf.reallocate(
-          [tempContractHandle, userOfferHandle],
-          [
-            zcf.getCurrentAllocation(userOfferHandle),
-            zcf.getCurrentAllocation(tempContractHandle),
-          ],
-        );
-        zcf.complete([tempContractHandle, userOfferHandle]); // FIXME: enough just one of them?
-
-        var give = {};
-        give['Wrapper' + nonce] = lockedAmount;
-    });
-
-  };
-
-  const { inviteAnOffer } = makeZoeHelpers(zcf);   
-
-  const adminHook = userOfferHandle => {
-  }
-
-  const makeSendInvite = (nonce, receiver, date) => () =>
-    inviteAnOffer(
-      harden({
-        offerHook: sendHook(nonce, receiver, date),
-        customProperties: { inviteDesc: 'offer' },
-      }),
+  const makeAddAssetsInvite = () =>
+    zcf.makeInvitation(
+      makeClaimAssetsInvite,
+      harden({ inviteDesc: 'addAssets' }),
     );
 
   return harden({
-    invite: zcf.makeInvitation(adminHook),
-    publicAPI: {
-      createToken,
-      makeSendInvite,
-      futureIssuer: issuer0,
-    },
+    invite: makeAddAssetsInvite(),
   });
 });
--- contract/test/test-proxy.js
+++ contract/test/test-proxy.js
@@ -1,164 +1,228 @@
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// import { test } from 'tape-promise/tape';
+// import bundleSource from '@agoric/bundle-source';
+// import harden from '@agoric/harden';
+// import produceIssuer from '@agoric/ertp';
+// import { E } from '@agoric/eventual-send';
+// import { makeZoe } from '@agoric/zoe';
+// import buildManualTimer from '@agoric/zoe/tools/manualTimer'
+
+// const contractRoot = `${__dirname}/../src/contracts/proxy.js`;
+
+// test(`Time release contract`, async t => {
+//   // Setup initial conditions
+//   const zoe = makeZoe({ require });
+//   const inviteIssuer = zoe.getInviteIssuer();
+
+//   const timerService = buildManualTimer(console.log);
+ 
+//   const contractReadyP = bundleSource(contractRoot).then(
+//     ({ source, moduleFormat }) => {
+//       const installationHandle = zoe.install(source, moduleFormat);
+
+//       return zoe
+//         .makeInstance(installationHandle, {}, { timerService })
+//         .then(myInvite => {
+//           return inviteIssuer
+//             .getAmountOf(myInvite)
+//             .then(({ extent: [{ instanceHandle: auditoriumHandle }] }) => {
+//               const { publicAPI } = zoe.getInstanceRecord(auditoriumHandle);
+
+//               return (
+//                 zoe
+//                   .offer(myInvite, harden({}))
+//                   // cancel will be renamed complete: https://github.com/Agoric/agoric-sdk/issues/835
+//                   // cancelObj exists because of a current limitation in @agoric/marshal : https://github.com/Agoric/agoric-sdk/issues/818
+//                   .then(
+//                     async ({
+//                       // outcome: outcomeP,
+//                       payout,
+//                       // cancelObj: { cancel: complete },
+//                       // offerHandle,
+//                     }) => {
+//                       // const amount = await E(publicAPI.issuer).getAmountOf((await payout).Wrapper); // necessary to wait for payout
+
+//                       return {
+//                         publicAPI,
+//                         // operaPayout: payout,
+//                         // complete,
+//                       };
+//                     },
+//                   )
+//               );
+//             });
+//         });
+//     },
+//   )
+
+//   contractReadyP.then(({ publicAPI }) => {
+//     const currencyIssuer = produceIssuer('BaytownBucks')
+//     const { mint: baytownBucksMint, issuer } = currencyIssuer;
+//     const baytownBucks = issuer.getAmountMath().make;
+
+//     const amount = baytownBucks(1000);
+//     const payment = baytownBucksMint.mintPayment(amount);
+
+//     async function pushPullMoney(date, positive) {
+//       const { issuer: wrapperIssuer, nonce } = await publicAPI.createToken(issuer);
+
+//       let future = null;
+
+//       const bob = {
+//         receivePayment: async (futurePayment) => {
+//           future = futurePayment;
+//           // console.log(amount)
+//           // const amount = await E(publicAPI.futureIssuer).getAmountOf(futurePayment); // FIXME: uncomment
+//           // const timeRelease = amount.extent[0][0];
+
+//           // const expectedAmount = await timeRelease.getAmount();
+//           // t.equal(expectedAmount.extent, 1000, `correct expected payment amount`);
+//           // t.equal(timeRelease.getIssuer().getBrand().getAllegedName(), 'BaytownBucks', 'payment was in BaytownBucks');
+
+//           // const realPayment = await timeRelease.getPayment();
+//           // if(!positive) {
+//           //   t.equal(realPayment, null, `There is no payment yet.`);
+//           // } else {
+//           //   t.equal((await issuer.getAmountOf(realPayment)).extent, 1000, `correct payment amount`);
+//           //   // Now the payment can be deposited.
+//           // }
+
+//           return {
+//             publicAPI,
+//           };
+//         }
+//       };
+
+//       const sendInvite = inviteIssuer.claim(publicAPI.makeSendInvite(
+//         harden(nonce), harden(bob), harden(date))());
+
+//       const alice = () => {
+//         return zoe
+//           .offer(sendInvite, harden(aliceProposal), {})
+//           .then(
+//             async ({
+//               // outcome: outcomeP,
+//               payout,
+//               // cancelObj: { cancel: complete },
+//               // offerHandle,
+//             }) => {
+//               // const amount = await E(publicAPI.issuer).getAmountOf((await payout).Wrapper); // necessary to wait for payout
+//               // console.log(amount);
+
+//               return {
+//                 publicAPI,
+//               };
+//             },
+//           )
+//           .then(() => {
+//             return { publicAPI };
+//           });
+//       }
+
+//       const give = {};
+//       give['Wrapper' + nonce] = amount;
+//       var paymentDesc = {};
+//       paymentDesc['Wrapper' + nonce] = payment;
+//       const aliceProposal = { give: give };
+
+//       // console.log(payment.getAllegedBrand().getAllegedName());
+
+//       // FIXME: This operator prints "payment not found for BaytownBucks"
+//       return zoe
+//         .offer(sendInvite, harden(aliceProposal), harden(paymentDesc))
+//         .then(
+//           async ({
+//             // outcome: outcomeP,
+//             payout,
+//             // cancelObj: { cancel: complete },
+//             // offerHandle,
+//           }) => {
+//             // const amount = await E(publicAPI.issuer).getAmountOf((await payout).Wrapper); // necessary to wait for payout
+//             // console.log(amount);
+
+//             return {
+//               publicAPI,
+//             };
+//           },
+//         )
+//         .then(() => {
+//           return { publicAPI };
+//         });
+//     }
+
+//     return pushPullMoney(1, false)
+//     //   .then(async (x) => {
+//     //     await E(timerService).tick("Going to the future");
+//     //     return pushPullMoney(1, true);
+//     //   });
+//   })
+//   .catch(err => {
+//     console.error('Error in last Time Release part', err);
+//     t.fail('  error');
+//   })
+//   .then(() => t.end());
+// });
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { test } from 'tape-promise/tape';
 import bundleSource from '@agoric/bundle-source';
 import harden from '@agoric/harden';
-import produceIssuer from '@agoric/ertp';
 import { E } from '@agoric/eventual-send';
+import produceIssuer from '@agoric/ertp';
+
 import { makeZoe } from '@agoric/zoe';
-import buildManualTimer from '@agoric/zoe/tools/manualTimer'
 
-const contractRoot = `${__dirname}/../src/contracts/proxy.js`;
+const timeReleaseRoot = `${__dirname}/../src/contracts/proxy.js`;
 
-test(`Time release contract`, async t => {
-  // Setup initial conditions
-  const zoe = makeZoe({ require });
-  const inviteIssuer = zoe.getInviteIssuer();
+test.only('zoe - time release', async t => {
+  t.plan(1);
+  try {
+    const zoe = makeZoe({ require });
+    // Pack the contract.
+    const { source, moduleFormat } = await bundleSource(timeReleaseRoot);
+    const installationHandle = await E(zoe).install(source, moduleFormat);
 
-  const timerService = buildManualTimer(console.log);
- 
-  const contractReadyP = bundleSource(contractRoot).then(
-    ({ source, moduleFormat }) => {
-      const installationHandle = zoe.install(source, moduleFormat);
+    const { mint, issuer, amountMath } = produceIssuer('aliceBucks');
 
-      return zoe
-        .makeInstance(installationHandle, {}, { timerService })
-        .then(myInvite => {
-          return inviteIssuer
-            .getAmountOf(myInvite)
-            .then(({ extent: [{ instanceHandle: auditoriumHandle }] }) => {
-              const { publicAPI } = zoe.getInstanceRecord(auditoriumHandle);
+    // TODO: make real timer obj
+    const timer = {};
 
-              return (
-                zoe
-                  .offer(myInvite, harden({}))
-                  // cancel will be renamed complete: https://github.com/Agoric/agoric-sdk/issues/835
-                  // cancelObj exists because of a current limitation in @agoric/marshal : https://github.com/Agoric/agoric-sdk/issues/818
-                  .then(
-                    async ({
-                      // outcome: outcomeP,
-                      payout,
-                      // cancelObj: { cancel: complete },
-                      // offerHandle,
-                    }) => {
-                      // const amount = await E(publicAPI.issuer).getAmountOf((await payout).Wrapper); // necessary to wait for payout
+    // Alice creates a contract instance
+    const addAssetsInvite = await E(zoe).makeInstance(
+      installationHandle,
+      { Token: issuer },
+      { timer },
+    );
 
-                      return {
-                        publicAPI,
-                        // operaPayout: payout,
-                        // complete,
-                      };
-                    },
-                  )
-              );
-            });
-        });
-    },
-  )
+    // Alice adds assets
+    const tokens1000 = amountMath.make(1000);
+    const bucksPayment = mint.mintPayment(tokens1000);
+    const aliceProposal = harden({
+      give: { Token: tokens1000 },
+      // she will not be able to exit on her own. We could also have a
+      // deadline that is after the expected timed release of the funds.
+      exit: { waived: null },
+    });
+    const { outcome: bobInvite } = await E(zoe).offer(
+      addAssetsInvite,
+      aliceProposal,
+      { Token: bucksPayment },
+    );
 
-  contractReadyP.then(({ publicAPI }) => {
-    const currencyIssuer = produceIssuer('BaytownBucks')
-    const { mint: baytownBucksMint, issuer } = currencyIssuer;
-    const baytownBucks = issuer.getAmountMath().make;
+    // Bob tries to get the funds. Right now he can get them
+    // immediately because we didn't set up the timer
+    const { payout: payoutP } = await E(zoe).offer(bobInvite);
 
-    const amount = baytownBucks(1000);
-    const payment = baytownBucksMint.mintPayment(amount);
+    // Bob's payout promise resolves
+    const bobPayout = await payoutP;
+    const bobTokenPayout = await bobPayout.Token;
 
-    async function pushPullMoney(date, positive) {
-      const { issuer: wrapperIssuer, nonce } = await publicAPI.createToken(issuer);
+    const tokenPayoutAmount = await issuer.getAmountOf(bobTokenPayout);
 
-      let future = null;
-
-      const bob = {
-        receivePayment: async (futurePayment) => {
-          future = futurePayment;
-          // console.log(amount)
-          // const amount = await E(publicAPI.futureIssuer).getAmountOf(futurePayment); // FIXME: uncomment
-          // const timeRelease = amount.extent[0][0];
-
-          // const expectedAmount = await timeRelease.getAmount();
-          // t.equal(expectedAmount.extent, 1000, `correct expected payment amount`);
-          // t.equal(timeRelease.getIssuer().getBrand().getAllegedName(), 'BaytownBucks', 'payment was in BaytownBucks');
-
-          // const realPayment = await timeRelease.getPayment();
-          // if(!positive) {
-          //   t.equal(realPayment, null, `There is no payment yet.`);
-          // } else {
-          //   t.equal((await issuer.getAmountOf(realPayment)).extent, 1000, `correct payment amount`);
-          //   // Now the payment can be deposited.
-          // }
-
-          return {
-            publicAPI,
-          };
-        }
-      };
-
-      const sendInvite = inviteIssuer.claim(publicAPI.makeSendInvite(
-        harden(nonce), harden(bob), harden(date))());
-
-      const alice = () => {
-        return zoe
-          .offer(sendInvite, harden(aliceProposal), {})
-          .then(
-            async ({
-              // outcome: outcomeP,
-              payout,
-              // cancelObj: { cancel: complete },
-              // offerHandle,
-            }) => {
-              // const amount = await E(publicAPI.issuer).getAmountOf((await payout).Wrapper); // necessary to wait for payout
-              // console.log(amount);
-
-              return {
-                publicAPI,
-              };
-            },
-          )
-          .then(() => {
-            return { publicAPI };
-          });
-      }
-
-      const give = {};
-      give['Wrapper' + nonce] = amount;
-      var paymentDesc = {};
-      paymentDesc['Wrapper' + nonce] = payment;
-      const aliceProposal = { give: give };
-
-      // console.log(payment.getAllegedBrand().getAllegedName());
-
-      // FIXME: This operator prints "payment not found for BaytownBucks"
-      return zoe
-        .offer(sendInvite, harden(aliceProposal), harden(paymentDesc))
-        .then(
-          async ({
-            // outcome: outcomeP,
-            payout,
-            // cancelObj: { cancel: complete },
-            // offerHandle,
-          }) => {
-            // const amount = await E(publicAPI.issuer).getAmountOf((await payout).Wrapper); // necessary to wait for payout
-            // console.log(amount);
-
-            return {
-              publicAPI,
-            };
-          },
-        )
-        .then(() => {
-          return { publicAPI };
-        });
-    }
-
-    return pushPullMoney(1, false)
-    //   .then(async (x) => {
-    //     await E(timerService).tick("Going to the future");
-    //     return pushPullMoney(1, true);
-    //   });
-  })
-  .catch(err => {
-    console.error('Error in last Time Release part', err);
-    t.fail('  error');
-  })
-  .then(() => t.end());
+    // Bob got 1000 tokens
+    t.deepEquals(tokenPayoutAmount, tokens1000);
+  } catch (e) {
+    t.assert(false, e);
+    console.log(e);
+  }
 });
